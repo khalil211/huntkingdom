3b864278745e9b2b3aa2b46527ce6339a7f09e90
louay122 <louay.gourrida@esprit.tn>
2020-02-16 15:44:54 +0100

    blog update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new UserBundle\UserBundle(),
             new GestionProduitBundle\GestionProduitBundle(),
+            new BlogBundle\BlogBundle(),
             new CommandeBundle\CommandeBundle(),
         ];
 

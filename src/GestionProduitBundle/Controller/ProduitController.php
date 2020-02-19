8fa0445f11b5b91d2fa65a148db084ed51a8b22e
eya01 <eyahech@gmail.com>
2020-02-19 14:05:26 +0100

    pdf + métiers

--- src/GestionProduitBundle/Controller/ProduitController.php
+++ src/GestionProduitBundle/Controller/ProduitController.php
@@ -3,10 +3,16 @@
 namespace GestionProduitBundle\Controller;
 
 use GestionProduitBundle\Entity\CategorieProduit;
+use GestionProduitBundle\Entity\CommentaireProduit;
 use GestionProduitBundle\Entity\Produit;
-use http\Env\Response;
+use GestionProduitBundle\Form\CommentaireProduitType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\User;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Produit controller.
@@ -27,6 +33,30 @@ class ProduitController extends Controller
         return $this->render('produit/index.html.twig', array(
             'produits' => $produits,
         ));
+    }
+
+    public function listpAction(){
+
+        $em = $this->getDoctrine()->getManager();
+
+        $produits = $em->getRepository('GestionProduitBundle:Produit')->findAll();
+        $snappy = $this->get('knp_snappy.pdf');
+
+        $html = $this->renderView('produit/listp.html.twig', array(
+            'produits' => $produits,
+
+        ));
+
+        $filename = 'myFirstSnappyPDF';
+
+        return new Response(
+            $snappy->getOutputFromHtml($html),
+            200,
+            array(
+                'Content-Type'          => 'application/pdf',
+                'Content-Disposition'   => 'inline; filename="'.$filename.'.pdf"'
+            )
+        );
     }
 
     /**
@@ -129,9 +159,17 @@ class ProduitController extends Controller
     public function shopAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
+        $queryBuilder=$em->getRepository('GestionProduitBundle:Produit')->createQueryBuilder('p');
+        if ($request->query->getAlnum('filter')) {
+            $listeproduits=$queryBuilder->where('p.nomProd LIKE :nomProd')
+                            ->setParameter('nomProd', '%' . $request->query->getAlnum('filter') . '%')
+                            ->getQuery()
+                            ->getResult();
+        }
+        else
+            $listeproduits = $em->getRepository('GestionProduitBundle:Produit')->findAll();
         $em2=$this->getDoctrine()->getManager();
         $categories=$em2->getRepository('GestionProduitBundle:CategorieProduit')->findAll();
-         $listeproduits = $em->getRepository('GestionProduitBundle:Produit')->findAll();
          $produits  = $this->get('knp_paginator')->paginate(
         $listeproduits,
         $request->query->get('page', 1)/*le numéro de la page à afficher*/,
@@ -146,43 +184,58 @@ class ProduitController extends Controller
 
     }
 
-    public function detailsAction(Produit $produit){
+    public function detailsAction(Request $request,Produit $produit){
+
+        $user=$this->getUser();
+        if($user==null)
+            return $this->redirectToRoute('fos_user_security_login');
+        $add_comment = new CommentaireProduit();
+        $em = $this->getDoctrine()->getManager();
+        $comments=$em->getRepository(CommentaireProduit::class)->findByProduit($produit);
+        $add_comment->setProduit($produit);
+        $add_comment->setUser($user);
+        $add_comment->setDateC( new \DateTime());
+        $form = $this->createFormBuilder($add_comment)
+
+            ->add('contenu', TextareaType::class)
+
+            ->getForm();
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $add_comment = $form->getData();
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($add_comment);
+                $em->flush();
+                return $this->redirectToRoute('produit_details', array('id' => $produit->getId()));
+//notification
+            }
+        }
 
         return $this->render('produit/details.html.twig', array(
             'produit' => $produit,
+            'form' => $form->createView(),
+            'comment' => $add_comment,
+            'comments' => $comments,
             ));
     }
-    public function categoriesAction($id)
+    public function categoriesAction(Request $request,$id)
     {
         $em = $this->getDoctrine()->getManager();
         $em2=$this->getDoctrine()->getManager();
         $categories=$em2->getRepository('GestionProduitBundle:CategorieProduit')->findAll();
-        $produits = $em->getRepository('GestionProduitBundle:Produit')->findByCategorie( $em->getRepository(CategorieProduit::class)->find($id));
-
+        $listeproduits = $em->getRepository('GestionProduitBundle:Produit')->findByCategorie( $em->getRepository(CategorieProduit::class)->find($id));
+        $produits  = $this->get('knp_paginator')->paginate(
+            $listeproduits,
+            $request->query->get('page', 1)/*le numéro de la page à afficher*/,
+            5/*nbre d'éléments par page*/
+        );
         return $this->render('produit/shop.html.twig', array(
             'produits' => $produits,
             'categories'=>$categories,
         ));
     }
 
-  /*  public function searchProdAction(Request $request){
-        $em=$this->getDoctrine()->getManager();
-        $requestString = $request->get('q');
-        $produits=$em->getRepository('GestionProduitBundle:Produit')->findEntitiesByString($requestString);
-        if (!$produits){
-            $result['produits']['error']="Produit introuvable";
-        }
-        else{
-            $result['produits']=$this->getRealEntites($produits);
-
-        }
-        return new Response(json_encode($result));
-    }
-
-    public function getRealEntites($produits){
-        foreach ($produits as $produits){
-        $realEntities[$produits->getId()]=[$produits->getImageProd(),$produits->getNomProd()];
-    }
-    return $realEntities;
-}*/
 }
